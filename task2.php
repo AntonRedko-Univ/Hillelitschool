b236460eb99fcb290598cf2b5c1f8c8edec01255
Anton <AntonRedko-Univ@users.noreply.github.com>
2021-03-12 19:43:50 +0200

    Edit task2.php

--- task2.php
+++ task2.php
@@ -1,5 +1,21 @@
 <?php
-$text = 'execute';
 
-$string = "ababdcbadbcabcbcadcbadbc";
-print_r(array_count_values(str_split($text)));
+$string = 'Execute';
+$matches = array();
+$res = array();
+if(preg_match('#[a-z]+#i',$string)){
+    preg_match_all('/[a-z]+?/u',  $string, $matches);
+    foreach ($matches[0] as $match) {
+        $res[$match] = isset($res[$match]) ? ++$res[$match] : 1;
+    }
+
+    print_r($res);
+}
+elseif(preg_match('#[а-яё]+#i',$string)){
+    preg_match_all('/[а-яА-ЯёЁ]+?/u',  $string, $matches);
+    foreach ($matches[0] as $match) {
+        $res[$match] = isset($res[$match]) ? ++$res[$match] : 1;
+    }
+
+    print_r($res);
+}
