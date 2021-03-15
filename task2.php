2e7a33e22d04b04a2a06a623c8fcbef89091a696
Anton <AntonRedko-Univ@users.noreply.github.com>
2021-03-15 16:26:28 +0200

    Edited task2.php

--- task2.php
+++ task2.php
@@ -1,21 +1,18 @@
 <?php
 
-$string = 'Execute';
+$string = 'axe топор';
 $matches = array();
 $res = array();
-if(preg_match('#[a-z]+#i',$string)){
+
     preg_match_all('/[a-z]+?/u',  $string, $matches);
     foreach ($matches[0] as $match) {
         $res[$match] = isset($res[$match]) ? ++$res[$match] : 1;
     }
 
-    print_r($res);
-}
-elseif(preg_match('#[а-яё]+#i',$string)){
     preg_match_all('/[а-яА-ЯёЁ]+?/u',  $string, $matches);
     foreach ($matches[0] as $match) {
         $res[$match] = isset($res[$match]) ? ++$res[$match] : 1;
     }
 
     print_r($res);
-}
+
